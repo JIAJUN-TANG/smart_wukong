39c3926f520dae8fd23cf1e23bc3a39f0ed37386
Jiajun Tang <zhiju022486@163.com>
2026-03-31 14:38:24 +0800

    customized speech service

--- app/src/main/java/com/example/wukongstarter/DemoMasterService.kt
+++ app/src/main/java/com/example/wukongstarter/DemoMasterService.kt
@@ -1,5 +1,30 @@
 package com.example.wukongstarter
 
-import com.ubtrobot.mini.speech.framework.MicrophoneArrayService
+import android.util.Log
+import com.ubtechinc.mini.weinalib.wakeup.WeiNaWakeUpHelper
+import com.ubtrobot.master.service.MasterSystemService
 
-class DemoMasterService : MicrophoneArrayService()
+class DemoMasterService : MasterSystemService() {
+
+    override fun onServiceCreate() {
+        super.onServiceCreate()
+        Log.d(TAG, "DemoMasterService onServiceCreate, initializing WeiNaWakeUpHelper...")
+
+        // If you want to use the WeiNa wake-up module(wakeup-5.0.0.aar),
+        // please call the initialization method first.
+        // The initialization of the wake-up module requires an available network.
+        WeiNaWakeUpHelper.get().initialize()
+
+        // init speech modules
+        DemoSpeech.init(this)
+    }
+
+    override fun onServiceDestroy() {
+        super.onServiceDestroy()
+        Log.d(TAG, "DemoMasterService onServiceDestroy")
+    }
+
+    companion object {
+        private const val TAG = "DemoMasterService"
+    }
+}
--- app/src/main/java/com/example/wukongstarter/WukongApp.kt
+++ app/src/main/java/com/example/wukongstarter/WukongApp.kt
@@ -1,6 +1,8 @@
 package com.example.wukongstarter
 
 import android.content.Intent
+import android.os.SystemClock
+import android.util.Log
 import com.ubtrobot.mini.speech.framework.AbstractSpeechApplication
 import com.ubtrobot.speech.SpeechService
 import com.ubtrobot.speech.SpeechSettings
@@ -10,16 +12,28 @@ class WukongApp : AbstractSpeechApplication() {
     override fun onCreate() {
         super.onCreate()
 
+        // Start DemoMasterService first - it calls DemoSpeech.init(service)
+        // which initializes WakeUpDetector and creates CompositeSpeechService
+        startService(Intent(this, DemoMasterService::class.java))
+
         ServiceModules.declare(SpeechSettings::class.java) { _, notifier ->
-            notifier.notifyModuleCreated(DemoSpeech.INSTANCE.createSpeechSettings(this))
+            notifier.notifyModuleCreated(DemoSpeech.createSpeechSettings(this))
         }
 
+        // Speech service is created asynchronously in DemoSpeech.init(),
+        // so we need to wait for it on a background thread (following official demo pattern)
         ServiceModules.declare(SpeechService::class.java) { _, notifier ->
-            notifier.notifyModuleCreated(DemoSpeech.INSTANCE.getSpeechService(this))
+            Thread {
+                while (DemoSpeech.createSpeechService() == null) {
+                    SystemClock.sleep(5)
+                }
+                Log.d("WukongApp", "Speech Service create ok..")
+                notifier.notifyModuleCreated(DemoSpeech.createSpeechService())
+            }.start()
         }
 
         try {
-            startService(Intent(this, DemoMasterService::class.java))
+            startService(Intent(this, WukongService::class.java))
         } catch (e: Exception) {
             e.printStackTrace()
         }
